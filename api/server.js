0e4632f548ff996d03a8a50c0fc16cd5be7d57d8
Brett Karr <b.karr@polyclip.us>
2020-02-22 13:22:13 -0600

    authentication and restriction to jokesRouter is working

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const configureMiddleware = require("./configureMiddleware.js");
 
-const { authenticate } = require("../auth/authenticate-middleware.js");
+const { restricted } = require("../auth/authenticate-middleware.js");
 const authRouter = require("../auth/auth-router.js");
 const jokesRouter = require("../jokes/jokes-router.js");
 
@@ -9,8 +9,8 @@ const server = express();
 configureMiddleware(server);
 
 server.use("/api/auth", authRouter);
-server.use("/api/jokes", jokesRouter);
-// server.use("/api/jokes", authenticate, jokesRouter);
+// server.use("/api/jokes", jokesRouter);
+server.use("/api/jokes", restricted, jokesRouter);
 
 server.get("/api", (req, res) => {
   res.json({ message: `API Working!!!!!` });
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -5,13 +5,13 @@
 
 const users = require("../utils/db-model.js");
 const bcrypt = require("bcryptjs");
-// const secrets = require("../utils/secrets.js");
-// const jwt = require("jsonwebtoken");
+const secrets = require("../utils/secrets.js");
+const jwt = require("jsonwebtoken");
 
 exports.authenticate = (req, res, next) => {
   const { username, password } = req.body;
   // const { username, password } = req.headers;
-  console.log("authenticate:", username, password);
+  console.log("authenticate:", req.body, req.session, req.headers);
   // validate that they exist ... we didn't have this part in class...
   if (!(username && password)) {
     res.status(401).json({ message: "invalid Inputs" });
@@ -53,5 +53,31 @@ exports.verifyNewUser = (req, res, next) => {
     res
       .status(400) //error
       .json({ message: "username and password fields required" });
+  }
+};
+
+exports.restricted = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (token) {
+    console.log("restricted: token exists:\n");
+    jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+      if (err) {
+        console.log("restricted: jwt.verify error:\n", err, decodedToken);
+        res
+          .status(401)
+          .json({ message: "authorization failed. Token is different" });
+      } else {
+        console.log("restricted: jwt.verify no error:\n", decodedToken);
+        if (req.params.id) {
+          if (decodedToken.subject == req.params.id) {
+            next();
+          } else {
+            res.status(401).json({ message: "You do not have access" });
+          }
+        } else {
+          next();
+        }
+      }
+    });
   }
 };
